c3a1a379041341f10fd3f0a5cb05b56ea93978f6
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-09-22 18:54:29 +0000

    added testing for add to cart feature

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+before :each do
+  @category = Category.create! name: 'Apparel'
+
+  10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+end
+
+  scenario "They see all products" do
+    # ACT
+    
+    visit root_path
+
+    
+    click_on('Add', match: :first)
+
+    find('a', text: 'My Cart (1)', exact: true)
+
+
+    # DEBUG / VERIFY
+    save_screenshot
+
+
+  end
+end
